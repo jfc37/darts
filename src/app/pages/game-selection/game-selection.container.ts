d5b6f05f3e9ab5f9dd572b98b22c7d80462d775d
Joe Chapman <joe.chapman@outlook.co.nz>
2025-03-26 21:29:03 +1300

    add deploy script and update routing in game selection component

--- src/app/pages/game-selection/game-selection.container.ts
+++ src/app/pages/game-selection/game-selection.container.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-game-selection',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './game-selection.container.html',
   styleUrl: './game-selection.container.scss'
 })
